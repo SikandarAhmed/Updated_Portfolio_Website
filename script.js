41bb669712114346223ec63a5c5bf39a602ceab9
SikandarAhmed <156305460+SikandarAhmed@users.noreply.github.com>
2024-03-08 13:40:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+
+<script>
+    var typed = new Typed('#element', {
+        strings: ['Web Developer', 'Content Writer', 'Script Writer', 'Web Designer', 'Web Developer'],
+        typeSpeed: 160,
+    });
+</script>
